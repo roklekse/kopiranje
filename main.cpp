66bebbf0eef4cda0e58c0621de72378b82b9f904
roklekse <kren470@gmail.com>
2014-07-05 18:44:46 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+//200 MB bo ostalo prostega RAM-a
+#define PROST_SPOMIN 209715200
+
+using namespace std;
+
+
+/*
+  Ta funkcija ugotovi, koliko je prostega spomina v računalniku.
+  Nato naredi char array, ki je velik (največ kolikor je mogoče) - PROST_SPOMIN.
+  
+*/
+void alokacijaRAM()
+{
+
+
+}
+
+
+/*
+  Ta funkcija prebere binarno datoteko.
+*/
+void binarnoBranje()
+{
+
+}
+
+/*
+  Ta funkcija zapiše binarno datoteko
+*/
+void binarnoPisanje()
+{
+
+}
+
+/*
+  Sprehajanje se po trenutnem direktoriju (pot) in vseh poddirektorijih
+*/
+void sprehod(string pot)
+{
+
+}
+
+int main()
+{
+  
+  return 0;
+}
